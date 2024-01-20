0a70d27da3ca1779fb47558ca23fa72cb3006425
jonaturn <jonathencheng86@gmail.com>
2024-01-21 07:43:37 +0800

    username on homebar

--- frontend/lib/features/home/home_appbar.dart
+++ frontend/lib/features/home/home_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/common/appbar/appbar.dart';
@@ -6,6 +7,18 @@ class HomeAppBar extends StatelessWidget {
   HomeAppBar({
     super.key,
   });
+
+  String? username;
+
+  Future<void> getUsername() async {
+    
+    await FirebaseFirestore.instance
+    .collection('users')
+    .doc(FirebaseAuth.instance.currentUser!.email.toString())
+    .get()
+    .then((value) => username = value['username']);
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomAppBar(
@@ -15,11 +28,13 @@ class HomeAppBar extends StatelessWidget {
                 .textTheme
                 .labelMedium!
                 .apply(color: Color(0xFFE0E0E0))),
-        Text(FirebaseAuth.instance.currentUser!.email.toString(),
-            style: Theme.of(context)
+        FutureBuilder( 
+            future: getUsername(),
+            builder: (context, snapshot) => Text(username!,             style: Theme.of(context)
                 .textTheme
                 .headlineSmall!
-                .apply(color: Color(0xFFFFFFFF)))
+                .apply(color: Color(0xFFFFFFFF)))),
+
       ]),
       actions: [
         //IconButton(onPressed: (){}, icon: const Icon(Icons.search_outlined, color: Color(0xFFFFFFFF)))
