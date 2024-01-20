d4d078382639eef57623bde16a9c8ec9adc48728
jonaturn <jonathencheng86@gmail.com>
2024-01-20 21:14:58 +0800

    Post page

--- frontend/lib/main_pages/listing_page.dart
+++ frontend/lib/main_pages/listing_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ListingPage extends StatefulWidget {
@@ -9,10 +11,39 @@ class ListingPage extends StatefulWidget {
 
 class _ListingPageState extends State<ListingPage> {
   //Firebase document IDs
+  List<Map<String, dynamic>> listData = [];
+  List<String> docIDList = [];
+  String? docID;
+
+  Future getDocID() async {
+    await FirebaseFirestore.instance
+        .collection('listing')
+        .get()
+        .then((snapshot) {
+      return snapshot.docs.forEach((document) {
+        if (document.data().containsValue(
+            FirebaseAuth.instance.currentUser!.email.toString())) {
+          listData.add(document.data());
+          docIDList.add(document.id);
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF4b68ff), body: Text('Listing Page'));
+        body: FutureBuilder(
+      future: getDocID(),
+      builder: (context, snapshot) {
+        return ListView.builder(
+            itemCount: listData.length,
+            itemBuilder: (context, index) => Text(
+                  listData[index].toString() + ", " +
+                  docIDList[index]
+                ));
+      },
+    ));
   }
 }
+
